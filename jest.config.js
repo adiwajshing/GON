b0ada618152209f4291fa6116538eb4feef770f4
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2021-04-11 16:09:43 +0530

    Add tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,15 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/build/', '/coverage/', '/docs/'],
 	"roots": [
 		"<rootDir>/src"
 	],
 	"testMatch": [
-		"**/tests/test.*.+(ts|tsx|js)",
+		"**/tests/test.*.ts",
 	],
 	"transform": {
 		"^.+\\.(ts|tsx)$": "ts-jest"
-	},
+  },
+  collectCoverageFrom: ['src/**/*'],
 }
---
+++ tests/test.encoder.ts
@@ -0,0 +1,50 @@
+import { Terminals } from "../src/types";
+import GON from "../src";
+
+const terms: Terminals = {
+  arrayStart: 0xF0,
+  arrayEnd: 0xF1,
+
+  objectStart: 0xF2,
+  objectEnd: 0xF3,
+  
+  byte: 0xFA,
+  short: 0xFB,
+  int: 0xFC,
+  long: 0xFD,
+
+  double: 0xFE,
+  float: 0xFF,
+
+  booleanTrue: 0xF4,
+  booleanFalse: 0xF5,
+
+  string: 0xF6,
+
+  null: 0xD0,
+  backspace: 0xD8,
+  htab: 0xD9,
+  linefeed: 0xDA,
+  formfeed: 0xDC,
+  carriagereturn: 0xDD
+};
+
+let parser;
+
+beforeAll(() => {
+  parser = GON({
+    terminals: terms
+  });
+});
+
+describe('Encoder', () => {
+  it('Should encode null', () => {
+    let inputData = null;
+    const expected = Buffer.from([0xD0]);
+    const actual = GON({
+      terminals: terms
+    }).encode(inputData);
+
+    expect(actual).toEqual(expected);
+  });
+});
