573e3cd2a7e2c05d3ea77ec2607a472f9fe88a9a
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2021-04-18 14:53:58 +0530

    Change number datattype and add limits to encoder

--- src/encode.ts
+++ src/encode.ts
@@ -14,6 +14,34 @@ export default (config: Config, obj: any, encArr: number[] = []) => {
     push(config.terminals.string)
     push(...arr)
     push(0)
+  } else if (typeof obj == 'number') {
+    // Check if number can be converted to IEEE754 without precision-loss
+    if (Number.isSafeInteger(obj)) {
+      if (obj < 0) {
+        if (obj >= -0x7F) { // int8
+          // dataview
+        } else if (obj >= -0x7FFF) { // int16
+          // dataview
+        } else if (obj >= -0x7FFFFFFF) { // int32
+          // dataview
+        } else { // int64
+          // dataview
+        }
+      } else {
+        if (obj <= 0xFF) { // uint8
+          // dataview
+        } else if (obj <= 0xFFFF) { // uint16
+          // dataview
+        } else if (obj <= 0xFFFFFFFF) { // uint32
+          // dataview
+        } else { // uint64
+          // dataview
+        }
+      }
+    } else { // float
+      // dataview
+    }
+    // TODO: handle larger numbers
   } else if(obj === null) {
     push(config.terminals.null)
   }
--- src/types.ts
+++ src/types.ts
@@ -8,9 +8,14 @@ export type Terminals = {
 	objectEnd: number
 	
 	byte: number
-	short: number
-	int: number
-	long: number
+  int8: number
+  uint8: number
+  int16: number
+  uint16: number
+  int32: number
+  uint32: number
+  int64: number
+  uint64: number
 
 	float: number
 	double: number
